b094f01ce70aa9e891fe1655ba21b51d8927e0ec
phyothinzaraung <phyothinzar.ag92@gmail.com>
2020-06-15 11:46:50 +0630

    very first time

---
+++ Win/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Win.Models;
+
+namespace Win.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Select([DataSourceRequest]DataSourceRequest request)
+        {
+            var data = Enumerable.Range(1, 10)
+                .Select(index => new Seeed_Rec
+                {
+                    ProductID = index,
+                    ProductName = "Product #" + index,
+                    UnitPrice = index * 10,
+                    Discontinued = false
+                });
+
+            return Json(data.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Win/Models/SeedModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Win.Models
+{
+    public class Seeed_Rec
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public bool Discontinued { get; set; }
+    }
+    public class SeedModel
+    {
+    }
+}
